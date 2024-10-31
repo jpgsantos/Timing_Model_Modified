42466fb1ffe7ad2387d2bc2f861470703215fe20
João Pedro Gomes dos Santos <jpgs_jpgs@hotmail.com>
2024-10-31 16:28:49 +0100

    Initial commit

---
+++ Matlab/Input_functions/Timing_Model_Modified_input3_Ca.m
@@ -0,0 +1,27 @@
+function thisAmp = Timing_Model_Modified_input3_Ca(times)
+persistent CaX
+persistent CaT
+persistent Cah1
+persistent Cah2
+if isempty(CaX)
+Data = coder.load('C:/Users/Santos/Documents/GitHub/Thesis_Code/Subcellular_workflow/Matlab/Model/Timing_Model_Modified/Matlab/Data/Input_Timing_Model_Modified.mat', 'exp3_Ca');
+CaX = Data.exp3_Ca(:, 2);
+CaT = Data.exp3_Ca(:, 1);
+Cah2 = CaT(2)-CaT(1);
+Cah1 = 1;
+thisAmp = CaX(1);
+elseif times <= 0
+thisAmp = CaX(1);
+elseif times >= 20
+thisAmp = CaX(end);
+else
+while times > CaT(Cah1)
+Cah1 = Cah1 + 1;
+end
+while times < CaT(Cah1-1)
+Cah1  = Cah1-1;
+end
+Cah3 = (CaT(Cah1)-times)/Cah2;
+thisAmp = CaX(Cah1-1)*Cah3 + CaX(Cah1)*(1-Cah3);
+end
+end
---
+++ Matlab/Input_functions/Timing_Model_Modified_input4_DA.m
@@ -0,0 +1,27 @@
+function thisAmp = Timing_Model_Modified_input4_DA(times)
+persistent DAX
+persistent DAT
+persistent DAh1
+persistent DAh2
+if isempty(DAX)
+Data = coder.load('C:/Users/Santos/Documents/GitHub/Thesis_Code/Subcellular_workflow/Matlab/Model/Timing_Model_Modified/Matlab/Data/Input_Timing_Model_Modified.mat', 'exp4_DA');
+DAX = Data.exp4_DA(:, 2);
+DAT = Data.exp4_DA(:, 1);
+DAh2 = DAT(2)-DAT(1);
+DAh1 = 1;
+thisAmp = DAX(1);
+elseif times <= 0
+thisAmp = DAX(1);
+elseif times >= 20
+thisAmp = DAX(end);
+else
+while times > DAT(DAh1)
+DAh1 = DAh1 + 1;
+end
+while times < DAT(DAh1-1)
+DAh1  = DAh1-1;
+end
+DAh3 = (DAT(DAh1)-times)/DAh2;
+thisAmp = DAX(DAh1-1)*DAh3 + DAX(DAh1)*(1-DAh3);
+end
+end
